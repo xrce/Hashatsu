c4b01ad5b6de8e4522e7c1452946991a0f4f932d
N1ght420 <n1ghtpe0ple@protonmail.com>
2022-08-04 09:55:50 +0700

    Update

--- hash.py
+++ hash.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests, os, argparse, hashlib, time
+import requests, os, argparse, hashlib
 
 parse = argparse.ArgumentParser()
 parse.add_argument('-m', help='crack method', dest='mode')
 parse.add_argument('-s', help='hash to crack', dest='hash')
+parse.add_argument('-l', help='hash list', dest='list')
 args = parse.parse_args()
 
 mode = args.mode
 hashtext = args.hash
+hashlist = args.list
 
 B  = '\033[1;34'
 C  = '\033[1;36m'
@@ -69,49 +71,72 @@ def hashbrute():
             hashes = generateHashFromString(hashtype, formatted_string)
             print(C+" "+hashes+W+" [ "+R+formatted_string+W+" ]", end="\r", flush=True)
             if hashes == hashtext:
-                print("\n")
-                print(C+" [+]"+W+" Result"+R+" : "+W+formatted_string)
+                if hashlist:
+                    print(C+"\r [+] "+W+hashtext+R+" : "+W+formatted_string)
+                else:
+                    print(C+"\n [+]"+W+" Result"+R+" : "+W+formatted_string)
                 break
     decrypt()
 
-def md5decrypt():
+def md5decrypt(mode):
     email = "bransen.vikranth@logdots.com"
     code = "49898fab6e014903"
     result = requests.get('https://md5decrypt.net/en/Api/api.php?hash=%s&hash_type=%s&email=%s&code=%s' % (hashtext, hashtype, email, code)).text
     if len(result) != 0:
-        print("")
-        print(C+" [+]"+W+" Result"+R+" : "+W+result)
+        if hashlist:
+            print(C+" [+] "+W+hashtext+R+" : "+W+result)
+        else:
+            print(C+" [+] "+W+"Result"+R+" : "+W+result)
     else:
         if mode == 'default':
-            print(Y+' //'+W+' I can\'t do it, trying bruteforce')
-            time.sleep(1)
-            print("")
+            if not hashlist:
+                print(Y+' //'+W+' I can\'t do it, trying bruteforce')
+                print("")
             hashbrute()
+
+def checktype(hashtext):
+    if len(hashtext) == 32:
+        return 'md5'
+    elif len(hashtext) == 40:
+        return 'sha1'
+    elif len(hashtext) == 64:
+        return 'sha256'
+    elif len(hashtext) == 96:
+        return 'sha384'
+    elif len(hashtext) == 128:
+        return 'sha512'
+    else:
+        return 'Not a valid Hash'
 
 banner()
 
-if len(hashtext) == 32:
-    hashtype = 'md5'
-elif len(hashtext) == 40:
-    hashtype = 'sha1'
-elif len(hashtext) == 64:
-    hashtype = 'sha256'
-elif len(hashtext) == 96:
-    hashtype = 'sha384'
-elif len(hashtext) == 128:
-    hashtype = 'sha512'
+if hashlist:
+    print(Y+' //'+W+' List : '+hashlist)
+    print("")
+    with open(hashlist) as hlist:
+        while (hashtext := hlist.readline().rstrip()):
+            hashtype = checktype(hashtext)
+            if hashtype != 'Not a valid Hash':
+                if hashtype and not mode:
+                    md5decrypt('default')
+                elif mode == 'brute' and hashtype:
+                    hashbrute()
+                elif mode == 'shot' and hashtype:
+                    md5decrypt()
+
 else:
-    print(hashtext+" Not a valid Hash")
-
-print(Y+' //'+W+' Hash : '+hashtext)
-print(Y+' //'+W+' Type : '+hashtype)
-print("")
-print(Y+' //'+W+' Trying to get a shot')
-
-if not mode:
-    mode = "default"
-    md5decrypt()
-elif mode == 'brute':
-    hashbrute()
-elif mode == 'shot':
-    md5decrypt()
+    hashtype = checktype(hashtext)
+    if hashtype != 'Not a valid Hash':
+        print(Y+' //'+W+' Hash : '+hashtext)
+        print(Y+' //'+W+' Type : '+hashtype)
+        print("")
+        if not mode:
+            print(Y+' //'+W+' Trying to get a shot\n')
+            md5decrypt('default')
+        elif mode == 'brute':
+            hashbrute()
+        elif mode == 'shot':
+            print(Y+' //'+W+' Trying to get a shot\n')
+            md5decrypt()
+    else:
+        print(hashtype)
