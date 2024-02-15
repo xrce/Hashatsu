d3a9feb0813968cb6358a009a57e72d0c86e16fa
Your Future Boyfriend <n1ghtpe0ple@protonmail.com>
2024-02-15 12:37:06 +0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import requests, os, argparse, hashlib
+import requests, os, argparse
+import hashlib as hs
 
 parse = argparse.ArgumentParser()
 parse.add_argument('-m', help='crack method', dest='mode')
@@ -10,10 +8,7 @@ parse.add_argument('-l', help='hash list', dest='list')
 parse.add_argument('-c', help='character set', dest='char')
 args = parse.parse_args()
 
-mode = args.mode
-hashtext = args.hash
-hashlist = args.list
-charset = args.char
+mode, hash, list, charset = args.mode, args.hash, args.list, args.char
 
 B  = '\033[1;34'
 C  = '\033[1;36m'
@@ -26,131 +21,91 @@ Y  = '\033[1;33m'
 
 def banner():
     os.system('clear')
-    print("")
-    print(R+"    ハ"+Y+"ッ"+G+"シ"+C+"ュ"+OG+"キ"+LG+"ラー"+W)
-    print("")
+    print(f"\n{R}    ハ{Y}ッ{G}シ{C}ュ{OG}キ{LG}ラー{W}\n")
 
-if not charset:
-    ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+if not charset: chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 else:
-    ascii_letters = ""
-    if 'a' in charset:
-        ascii_letters += "abcdefghijklmnopqrstuvwxyz"
-    if 'A' in charset:
-        ascii_letters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if '0' in charset:
-        ascii_letters += "0123456789"
-    if '!' in charset:
-        ascii_letters += "!\"\$%&/()=?-.:\\*'-_:.;,"
+    chars = ""
+    if '0' in charset: chars += "0123456789"
+    if 'a' in charset: chars += "abcdefghijklmnopqrstuvwxyz"
+    if 'A' in charset: chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if '!' in charset: chars += "!\"\$%&/()=?-.:\\*'-_:.;,"
 
-def hashbrute():
-    string_list = ascii_letters
-    
-    def generateHashFromString(hashtype, cleartextString):
-        if hashtype == "md5":
-            return hashlib.md5(cleartextString.encode()).hexdigest()
-        elif hashtype == "sha1":
-            return hashlib.sha1(cleartextString.encode()).hexdigest()
-        elif hashtype == "sha224":
-            return hashlib.sha224(cleartextString.encode()).hexdigest()
-        elif hashtype == "sha256":
-            return hashlib.sha256(cleartextString.encode()).hexdigest()
-        elif hashtype == "sha384":
-            return hashlib.sha384(cleartextString.encode()).hexdigest()
-        elif hashtype == "sha512":
-            return hashlib.sha512(cleartextString.encode()).hexdigest()
+def brute():
+    def ghash(type, str):
+        match type:
+            case "md5": return hs.md5(str.encode()).hexdigest()
+            case "sha1": return hs.sha1(str.encode()).hexdigest()
+            case "sha224": return hs.sha224(str.encode()).hexdigest()
+            case "sha256": return hs.sha256(str.encode()).hexdigest()
+            case "sha384": return hs.sha384(str.encode()).hexdigest()
+            case "sha512": return hs.sha512(str.encode()).hexdigest()
+            case _: pass
+    def rstring(string): return string[::-1]
+    def error(index_input):
+        if len(chars) <= index_input: pass
+        else: return chars[index_input]
+    def gstring(string):
+        if len(string) <= 0: string.append(chars[0])
         else:
-            pass
-    def reverseString(string):
-        return string[::-1]
-    def IndexErrorCheck(index_input):
-        if len(string_list) <= index_input:
-            pass
-        else:
-            return string_list[index_input]
-    def StringGenerator(string):
-        if len(string) <= 0:
-            string.append(string_list[0])
-        else:
-            # error checking needs to be done, otherwise a ValueError will raise
-            string[0] = IndexErrorCheck((string_list.index(string[0]) + 1) % len(string_list))
-            if string_list.index(string[0]) == 0:
-                return [string[0]] + StringGenerator(string[1:])
+            string[0] = error((chars.index(string[0]) + 1) % len(chars))
+            if chars.index(string[0]) == 0: return [string[0]] + gstring(string[1:])
         return string
     def decrypt():
-        generated_string = []
+        astring = []
         while True:
-            generated_string = StringGenerator(generated_string)
-            formatted_string = reverseString("".join(generated_string))
-            hashes = generateHashFromString(hashtype, formatted_string)
-            print(C+" "+hashes+W+" [ "+R+formatted_string+W+" ]", end="\r", flush=True)
-            if hashes == hashtext:
-                if hashlist:
-                    print(C+"\r [+] "+W+hashtext+R+" : "+W+formatted_string)
-                else:
-                    print(C+"\n [+] "+W+"Result"+R+" : "+W+formatted_string)
+            astring = gstring(astring)
+            fstring = rstring("".join(astring))
+            hashes = ghash(type, fstring)
+            print(f"{C} {hashes}{W} [ {R}{fstring}{W} ]", end="\r", flush=True)
+            if hashes == hash:
+                if list: print(f"\r{C} [+] {W}{hash}{R} : {W}{fstring}")
+                else: print(f"\n{C} [+] {W}Result{R} : {W}{fstring}")
                 break
     decrypt()
 
-def md5decrypt(mode):
-    email = "bransen.vikranth@logdots.com"
-    code = "49898fab6e014903"
-    result = requests.get('https://md5decrypt.net/en/Api/api.php?hash=%s&hash_type=%s&email=%s&code=%s' % (hashtext, hashtype, email, code)).text
-    if len(result) != 0:
-        if hashlist:
-            print(C+" [+] "+W+hashtext+R+" : "+W+result)
-        else:
-            print(C+" [+] "+W+"Result"+R+" : "+W+result)
-    else:
-        if mode == 'default':
-            if not hashlist:
-                print(Y+' //'+W+' I can\'t do it, trying bruteforce')
-                print("")
-            hashbrute()
+def shot(mode):
+    email = "arrow.chosen@farmoaks.com"
+    code = "a9da13def71735c0"
+    res = requests.get(f"https://md5decrypt.net/en/Api/api.php?hash={hash}&hash_type={type}&email={email}&code={code}").text
+    if len(res) != 0:
+        if list: print(f"{C} [+] {W}{hash}{R} : {W}{res}")
+        else: print(f"{C} [+] {W}Result{R} : {W}{res}")
+    elif mode == 'default':
+        if not list: print(Y+' //'+W+' I can\'t do it, trying bruteforce\n')
+        brute()
 
-def checktype(hashtext):
-    if len(hashtext) == 32:
-        return 'md5'
-    elif len(hashtext) == 40:
-        return 'sha1'
-    elif len(hashtext) == 64:
-        return 'sha256'
-    elif len(hashtext) == 96:
-        return 'sha384'
-    elif len(hashtext) == 128:
-        return 'sha512'
-    else:
-        return 'Not a valid Hash'
+def check(hash):
+    match len(hash):
+        case 32: return 'md5'
+        case 40: return 'sha1'
+        case 64: return 'sha256'
+        case 96: return 'sha384'
+        case 128: return 'sha512'
+        case _: return 'invalid'
 
 banner()
 
-if hashlist:
-    print(Y+' //'+W+' List : '+hashlist)
-    print("")
-    with open(hashlist) as hlist:
-        while (hashtext := hlist.readline().rstrip()):
-            hashtype = checktype(hashtext)
-            if hashtype != 'Not a valid Hash':
-                if hashtype and not mode:
-                    md5decrypt('default')
-                elif mode == 'brute' and hashtype:
-                    hashbrute()
-                elif mode == 'shot' and hashtype:
-                    md5decrypt()
-
+if list:
+    print(f"{Y} //{W} List : {list}\n")
+    with open(list) as hlist:
+        while (hash := hlist.readline().rstrip()):
+            type = check(hash)
+            if type != 'invalid':
+                if type and not mode: shot('default')
+                elif mode == 'brute' and type: brute()
+                elif mode == 'shot' and type: shot()
+            else: print(f"{R} [-] {W}{hash}{R} ({W}invalid{R})")
 else:
-    hashtype = checktype(hashtext)
-    if hashtype != 'Not a valid Hash':
-        print(Y+' //'+W+' Hash : '+hashtext)
-        print(Y+' //'+W+' Type : '+hashtype)
-        print("")
+    type = check(hash)
+    if type != 'invalid':
+        print(f"{Y} //{W} Hash : {hash}")
+        print(f"{Y} //{W} Type : {type}\n")
         if not mode:
-            print(Y+' //'+W+' Trying to get a shot\n')
-            md5decrypt('default')
-        elif mode == 'brute':
-            hashbrute()
+            print(f"{Y} //{W} Trying to get a shot\n")
+            shot('default')
+        elif mode == 'brute': brute()
         elif mode == 'shot':
-            print(Y+' //'+W+' Trying to get a shot\n')
-            md5decrypt()
-    else:
-        print(hashtype)
+            print(f"{Y} //{W} Trying to get a shot\n")
+            shot()
+    else: print(type)
